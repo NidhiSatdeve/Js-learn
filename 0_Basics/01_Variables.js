b5c32bbeaad0fbbaa17a1358ce8a02b98f8674d6
NidhiSatdeve <119127960+NidhiSatdeve@users.noreply.github.com>
2024-09-25 03:43:57 +0000

    Datatypes done

---
+++ 0_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+ const accountId = 144553
+ let accountEmail = "abc@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+let accountState;
+ //accountId = 2;
+ accountEmail = "hcg@gmail.com"
+ accountPassword = "67584"
+ accountCity ="Mumbai"
+ console.log(accountId);
+//  console.log(accountEmail);
+//  console.log(accountPassword);
+//  console.log(accountCity);
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState])
+ 
---
+++ 0_Basics/02_Datatypes.js
@@ -0,0 +1,13 @@
+let name = "String"
+let age = 18
+let isLoggedIn = false
+
+//null - standalone value
+// undefined
+// symbol to  find uniqueness
+
+//Object 
+console.log(typeof name);
+console.log(typeof "String");
+console.log(typeof null);
+console.log(typeof undefined);
