915be96b82b06957e28a33ea5958fbc61a23bade
NidhiSatdeve <119127960+NidhiSatdeve@users.noreply.github.com>
2024-09-25 04:17:53 +0000

    conversion done

--- 0_Basics/03_conversionOperation.js
+++ 0_Basics/03_conversionOperation.js
@@ -11,4 +11,18 @@ console.log(typeof isLoggedIn)
 console.log(typeof boolIsLoggedIn)
 console.log( boolIsLoggedIn)
 
+// ********************** Operations ********************************
 
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Nidhi"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
