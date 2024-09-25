f38f72800e451536844a9a44781170769a187c12
NidhiSatdeve <119127960+NidhiSatdeve@users.noreply.github.com>
2024-09-25 03:44:21 +0000

    conversion done

---
+++ 0_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = false
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+let isLoggedIn =  undefined;
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof isLoggedIn)
+console.log(typeof boolIsLoggedIn)
+console.log( boolIsLoggedIn)
+
+
